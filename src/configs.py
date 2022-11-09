c8083022f7305fdcfb3e85bb2f75b61acdbe1937
natan-alex <natancarmo321@gmail.com>
2022-11-09 20:16:48 -0300

    make model be saved in root folder

--- src/configs.py
+++ src/configs.py
@@ -10,8 +10,8 @@ image_height = 100
 assets_folder_path = os.path.join(_working_dir, "assets")
 fresh_images_folder_path = os.path.join(assets_folder_path, "Fresh")
 spoiled_images_folder_path = os.path.join(assets_folder_path, "Spoiled")
-saved_model_file_path = os.path.join(_working_dir, "saved_model", "saved_model.h5")
+saved_model_file_path = os.path.join(_working_dir, "saved_model.h5")
 
-tf.config.threading.set_inter_op_parallelism_threads(2) 
+tf.config.threading.set_inter_op_parallelism_threads(2)
 tf.config.threading.set_intra_op_parallelism_threads(2)
 tf.config.set_soft_device_placement(True)
